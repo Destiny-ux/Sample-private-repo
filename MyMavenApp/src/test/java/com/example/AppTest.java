48aa39326e06e3861b391d45b22563e7d957ec7f
Destiny-ux <deey40398@gmail.com>
2025-04-03 09:24:55 +0100

    Update AppTest.java

--- MyMavenApp/src/test/java/com/example/AppTest.java
+++ MyMavenApp/src/test/java/com/example/AppTest.java
@@ -20,7 +20,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
- class RegistrationServletTest {
+public class AppTest {
 
     private RegistrationServlet servlet;
 
